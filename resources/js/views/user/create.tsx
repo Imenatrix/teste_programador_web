59a8a48e0b8afb2f52fd282df157b6ca8491d92e
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-05-16 16:41:50 -0300

    adiciona estilo em user.create

--- resources/js/views/user/create.tsx
+++ resources/js/views/user/create.tsx
@@ -1,20 +1,55 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createUseStyles } from 'react-jss'
 
 const Create : React.FC = () => {
 
+    const styles = useStyles()
+
     return (
-        <div>
-            <form action="/user/store">
-                <input type="text" name="name"/> <br/>
-                <input type="text" name="email"/> <br/>
-                <input type="password" name="password"/> <br/>
-                <input type="submit" value="Registar"/>
+        <div className={styles.container}>
+            <form className={styles.form} action="/user/store">
+                <input className={styles.input} type="text" name="name"/> <br/>
+                <input className={styles.input} type="text" name="email"/> <br/>
+                <input className={styles.input} type="password" name="password"/> <br/>
+                <input className={styles.btnSubmit} type="submit" value="Registar"/>
             </form>
         </div>
     )
 
 }
+
+const useStyles = createUseStyles({
+    container : {
+        width : '100vw',
+        height : '100vh',
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    form : {
+        backgroundColor : 'coral',
+        padding : '1.5em',
+        borderRadius : 5
+    },
+    input : {
+        border : 'none',
+        outline : 'none',
+        padding : '1em',
+        borderRadius : 5,
+        marginBottom : '1em',
+        width : '20em'
+    },
+    btnSubmit : {
+        width : '100%',
+        padding : '1em',
+        borderRadius : 5,
+        backgroundColor : 'lightsalmon',
+        border : 'none',
+        color : 'white',
+        fontWeight : 'bold',
+    }
+})
 
 const root = document.getElementById('root')
 render(<Create/>, root)
