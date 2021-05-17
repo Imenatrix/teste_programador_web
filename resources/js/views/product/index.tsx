e20a3c82fb5be74a44615d2f8f480ab1d580060d
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-05-17 12:52:23 -0300

    Adiciona ProductList em product.index

--- resources/js/views/product/index.tsx
+++ resources/js/views/product/index.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Product from '../../interfaces/Product'
+import ProductList from '../../components/ProductList'
+import { createUseStyles } from 'react-jss'
 
 interface Props {
-    products : Array<any>
+    products : Array<Product>
 }
 
 const Index : React.FC<Props> = (props) => {
+
+    const styles = useStyles()
     
     return (
-        <div>
-            {props.products.map(product => (
-                <div key={product.id}>{product.name}</div>
-            ))}
+        <div className={styles.container}>
+            <ProductList products={props.products}/>
         </div>
     )
 
 }
 
+const useStyles = createUseStyles({
+    container : {
+        width : '100vw',
+        height : '100vh',
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center'
+    }
+})
+
 const root = document.getElementById('root')
-const products = JSON.parse(root?.getAttribute('products') || '[]')
+const products : Array<Product> = JSON.parse(root?.getAttribute('products') || '[]')
 render(<Index products={products}/>, root)
